51f56c4c26367a8e603377f3eef5ed05f4c6373a
Ben Newton <ben.newton112@gmail.com>
2023-08-28 07:56:28 +0100

    property page content done, needs structured

--- src/Pages/HomeDetailsPage/HomeDetailsPage.jsx
+++ src/Pages/HomeDetailsPage/HomeDetailsPage.jsx
@@ -2,7 +2,9 @@ import './HomeDetailsPage.css'
 import React from 'react'
 import { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
-
+import Header from './../../Components/Header/Header';
+import KeepInTouch from './../../Components/KeepInTouch/KeepInTouch';
+import Footer from './../../Components/Footer/Footer';
 import axios from 'axios'
 
 function HomeDetailsPage({clickedProperty}) {
@@ -23,11 +25,12 @@ function HomeDetailsPage({clickedProperty}) {
     }, []); // <-- Correct placement of the array of dependencies
 
   return (
-<div>
-  <button>Back to search</button>
+<div className='home-details-container'>
+  <Header/>
   {property && property.images && (
-        <div key={property._id}>
+        <div className='property-details'key={property._id}>
           <div className='property-images-container'>
+          <button>Back to search</button>
             <div className='property-main-image'>
               <img src={property.images[0]} alt='Main Property' />
             </div>
@@ -37,11 +40,73 @@ function HomeDetailsPage({clickedProperty}) {
               ))}
             </div>
           </div>
-          <p>{property.availability}</p>
+          <div className='property-info-container'>
+          <div className='property-info'>
+            <div className='property-address'>
+            <h2>{property.address.street}, {property.address.city}, {property.address.postcode}
+            <hr/></h2>
+            </div>
+            <div className='property-price'>
+  <div className='info-block bedrooms'>
+    <p>Bedrooms</p>
+    <h2>{property.bedroom_count}</h2>
+  </div>
+  <div className='info-block bathrooms'>
+    <p>Bathrooms</p>
+    <h2>{property.bathroom_count}</h2>
+  </div>
+  <div className='info-block property-type'>
+    <p>Property Type</p>
+    <h2>{property.property_type}</h2>
+  </div>
+  <div className='info-block rent'>
+    <p>Rent (pppw)</p>
+    <h2>£{property.rent / property.bedroom_count / 4}</h2>
+  </div>
+  <div className='info-block furnishing-type'>
+    <p>Furnished Type</p>
+    <h2>{property.furnished}</h2>
+  </div>
+  <div className='info-block available-from'>
+    <p>Available From</p>
+    <h2>{property.availability}</h2>
+  </div>
+</div>
+
+          </div>
+          <div className='property-buttons'>
+          <button className='property-shortlist'>Shortlist</button>
+          <button className='property-book'>Book Viewing</button>
+          </div>
+          </div>
+          <div className='description'>
+            <h1>Description</h1>
+            <p>{property.property_description}</p>
+          </div>
+          <div className='bedroom-prices-container'>
+            <h1>Bedroom Prices</h1>
+            <div className='bedroom-prices'>
+  {Object.entries(property.bedroom_prices).map(([bedroomName, price], index) => (
+    <div className='bedroom-price' key={bedroomName}>
+      <p>Bedroom {index + 1}:</p>
+      <p> £{price}</p>
+    </div>
+  ))}
+</div>
+
+            </div>
+  <div className='key-features-container'>
+    <h1>Key Features</h1>
+  {property.key_features.map((feature, index) => (
+    <h2 key={index}>{feature}</h2>
+  ))}
+
+  </div>
+  <div className='blank-div'></div>
         </div>
       )}
-
-  <p>{property.data}</p>
+  <KeepInTouch/>
+  <Footer/>
 </div>
       
   )
