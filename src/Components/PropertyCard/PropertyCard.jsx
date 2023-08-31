46bcb942e5d050d5be30b8e1ae1be5ae13980b58
Ben Newton <ben.newton112@gmail.com>
2023-08-31 22:10:06 +0100

    trying to fix

--- src/Components/PropertyCard/PropertyCard.jsx
+++ src/Components/PropertyCard/PropertyCard.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom';
-import {MdOutlineBed, MdOutlineBathtub,MdLocationOn, MdHomeFilled} from 'react-icons/md'
-import './PropertyCard.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdOutlineBed, MdOutlineBathtub, MdLocationOn, MdHomeFilled } from 'react-icons/md';
+import './PropertyCard.css';
 
-function PropertyCard({specificCity}) {
-  const { city_name, total, response } = specificCity;
-  const { clickedProperty, setClickedProperty} = useState('')
+function PropertyCard({ specificCity }) {
+  const { response } = specificCity;
+  const [clickedProperty, setClickedProperty] = useState('');
 
   const handleProperty = (_id) => {
-    // Do something with the clicked city name
-    
-    console.log('Clicked city:', _id);
+    // Do something with the clicked property ID
+    console.log('Clicked property:', _id);
     setClickedProperty(_id);
   };
 
   return (
     <div className='property-card-container'>
-{/* Display information for each property */}
-{response &&
+      {/* Display information for each property */}
+      {response &&
         response.map((property) => (
           <div key={property._id} className='property-card'>
-            <img src={property.images[0]}/>
+            <img src={property.images[0]} alt={`Property ${property._id}`} />
             <div className='rent-shower'>
               <div className='property-rent'>
-                <h3>£{property.rent/4}</h3>
+                <h3>£{property.rent / 4}</h3>
                 <p>pppw including bills</p>
               </div>
               <div className='bed-bath'>
-                <MdOutlineBed style={{ marginRight: '5px'}}/>
+                <MdOutlineBed style={{ marginRight: '5px' }} />
                 <p style={{ marginRight: '30px' }}>{property.bedroom_count}</p>
-                <MdOutlineBathtub style={{ marginRight: '5px' }}/>
+                <MdOutlineBathtub style={{ marginRight: '5px' }} />
                 <p>{property.bathroom_count}</p>
               </div>
             </div>
@@ -38,17 +37,23 @@ function PropertyCard({specificCity}) {
               <h3>{property.furnished}</h3>
             </div>
             <div className='address'>
-            <MdLocationOn style={{color:'rgba(58, 82, 149, 1)'}}/>
-            <p>{property.address.street}, {property.address.city}, {property.address.postcode}</p>
+              <MdLocationOn style={{ color: 'rgba(58, 82, 149, 1)' }} />
+              <p>
+                {property.address.street}, {property.address.city}, {property.address.postcode}
+              </p>
             </div>
             <div className='view-home-button'>
-            <Link to={`/properties/${property._id}`} style={{textDecoration: 'none'}}><p onClick={() => 
-            handleProperty(property._id)}><MdHomeFilled/>View Home</p></Link>
+              <Link to={`/properties/${property._id}`} style={{ textDecoration: 'none' }}>
+                <p onClick={() => handleProperty(property._id)}>
+                  <MdHomeFilled />
+                  View Home
+                </p>
+              </Link>
+            </div>
           </div>
-          </div>
-           ))}
+        ))}
     </div>
-  )
+  );
 }
 
-export default PropertyCard
+export default PropertyCard;
