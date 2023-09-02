ac57a75dd2dbd5884d27aa5ab6e6bd05a509f69e
Ben Newton <ben.newton112@gmail.com>
2023-09-02 10:29:28 +0100

    ?

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom', 'react-icons']
-    }
-  }
-})
+      external: ['react', 'react-dom', 'other-dependency'],
+    },
+  },
+});
