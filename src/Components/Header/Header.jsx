e637966ad48ed7740150cf09741b82c2eb7ef677
Ben Newton <ben.newton112@gmail.com>
2023-09-02 16:16:22 +0100

    hamburger done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MdOutlineHolidayVillage, MdOutlineFavoriteBorder, MdMailOutline, MdPostAdd } from 'react-icons/md';
+import { MdOutlineHolidayVillage, MdMenu, MdOutlineFavoriteBorder, MdMailOutline, MdPostAdd } from 'react-icons/md';
 import Modal from 'react-modal';
 import './Header.css'; // Import your CSS file
 
@@ -7,6 +7,7 @@ Modal.setAppElement(document.getElementById('root'));
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const customStyles = {
     content: {
@@ -31,9 +32,16 @@ function Header() {
         <MdOutlineHolidayVillage />
         <a href={'/'}>UniLife</a>
       </div>
-      <div className='header-buttons'>
-        <a><MdOutlineFavoriteBorder/> Shortlist</a>
-        <a onClick={() => setIsOpen(true)}><MdMailOutline/> Contact Us</a>
+
+      {/* Show the contact us and shortlist buttons on desktop screens */}
+      <div className={`header-buttons  ${showMobileMenu ? 'hide-on-desktop' : ''}`}>
+        <a><MdOutlineFavoriteBorder /> Shortlist</a>
+        <a onClick={() => setIsOpen(true)}><MdMailOutline /> Contact Us</a>
+      </div>
+
+            {/* Show the hamburger menu on mobile screens */}
+            <div className={`mobile-menu-icon ${showMobileMenu ? 'open' : ''}`} onClick={() => setShowMobileMenu(!showMobileMenu)}>
+        <MdMenu />
       </div>
 
       <Modal
@@ -43,45 +51,47 @@ function Header() {
         contentLabel="Contact Us Modal"
       >
         <div className='contact-us-modal-container'>
-        <div className='contact-us-modal-title-container'>
-          <div className='contact-us-modal-title'>
-          <h2 style={{fontWeight: '500', fontSize: '36px'}}>Contact Us</h2>
-          <p style={{width: '485px', lineHeight: '32px'}}>Feel free to contact us if you have any questions. Looking forward to hear from you.</p>
-          </div>
-          <div>
-            <MdPostAdd style={{fontSize: '70px', color: 'rgba(58, 82, 149, 1)'}}/>
-          </div>
+          <div className='contact-us-modal-title-container'>
+            <div className='contact-us-modal-title'>
+              <h2 style={{ fontWeight: '500', fontSize: '36px' }}>Contact Us</h2>
+              <p style={{ width: '485px', lineHeight: '32px' }}>Feel free to contact us if you have any questions. Looking forward to hearing from you.</p>
+            </div>
+            <div>
+              <MdPostAdd style={{ fontSize: '70px', color: 'rgba(58, 82, 149, 1)' }} />
+            </div>
           </div>
           <div className='contact-us-modal-input-left'>
-          <div className='contact-us-modal-input'>
-            <h2 style={{fontWeight: '500'}}>Name</h2>
-            <input type='text' placeholder='Enter your name'/>
-          </div>
-          <div className='contact-us-modal-input'>
-          <h2 style={{fontWeight: '500'}}>Phone Number</h2>
-            <input type='text' placeholder='Enter your phone number'/>
-          </div>
-          <div className='contact-us-modal-input'>
-          <h2 style={{fontWeight: '500'}}>Are you a...</h2>
-            <input type='dropdown' placeholder=''/>
-          </div>
+            <div className='contact-us-modal-input'>
+              <h2 style={{ fontWeight: '500' }}>Name</h2>
+              <input type='text' placeholder='Enter your name' />
+            </div>
+            <div className='contact-us-modal-input'>
+              <h2 style={{ fontWeight: '500' }}>Phone Number</h2>
+              <input type='text' placeholder='Enter your phone number' />
+            </div>
+            <div className='contact-us-modal-input'>
+              <h2 style={{ fontWeight: '500' }}>Are you a...</h2>
+              <input type='dropdown' placeholder='' />
+            </div>
           </div>
           <div className='contact-us-modal-input-right'>
-          <div className='contact-us-modal-input'>
-          <h2 style={{fontWeight: '500'}}>Email</h2>
-            <input type='text' placeholder='Enter your email address'/>
+            <div className='contact-us-modal-input'>
+              <h2 style={{ fontWeight: '500' }}>Email</h2>
+              <input type='text' placeholder='Enter your email address' />
+            </div>
+            <div className='contact-us-modal-input'
+              style={{ height: '232px' }}>
+              <h2 style={{ fontWeight: '500' }}>Message</h2>
+              <input type='text' placeholder='Enter your message'
+                style={{ height: '184px' }} />
+            </div>
+            <button className='modal-close-btn' onClick={() => setIsOpen(false)}
+              style={{
+                height: '56px', width: '400px', backgroundColor: 'rgba(58, 82, 149, 1)',
+                borderRadius: '12px', color: '#FFFFFF', fontSize: '20px', border: 'none', margin: '0 20px'
+              }}>Close</button>
           </div>
-          <div className='contact-us-modal-input'
-          style={{height: '232px'}}>
-          <h2 style={{fontWeight: '500'}}>Message</h2>
-            <input type='text' placeholder='Enter your message'
-            style={{height: '184px'}}/>
-          </div>
-          <button className='modal-close-btn' onClick={() => setIsOpen(false)}
-          style={{height: '56px', width: '400px', backgroundColor: 'rgba(58, 82, 149, 1)',
-          borderRadius: '12px', color: '#FFFFFF', fontSize: '20px', border: 'none', margin: '0 20px'}}>Close</button>
-          </div>
-          </div>
+        </div>
       </Modal>
     </div>
   );
